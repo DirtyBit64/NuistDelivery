32b097774b004a49244f9e771f7e0f458fc8ec4e
DirtyBit <ljx@nuist.edu.cn>
2024-02-24 21:31:43 +0800

    商品浏览业务开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -29,6 +29,11 @@ public interface DishFlavorMapper {
      */
     void deleteByIds(List<Long> dishIds);
 
+    /**
+     * 根据菜品id查询口味
+     * @param dishId
+     * @return
+     */
     @Select("select * from dish_flavor where dish_id = #{dishId}")
     List<DishFlavor> getByDishId(Long dishId);
 }
